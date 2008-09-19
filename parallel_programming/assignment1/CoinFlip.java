3b5979831b9529a04c4bb0ecbf3c2071feae6001
Terence Lee <hone02@gmail.com>
2008-09-19 18:10:19 -0400

    * corrected the number of iterations to be split across threads * added useful comments

--- parallel_programming/assignment1/CoinFlip.java
+++ parallel_programming/assignment1/CoinFlip.java
@@ -32,6 +32,9 @@ public class CoinFlip implements Runnable {
 		return this.totalRuns;
 	}
 
+	/**
+	 * method that is run when start() is called by Thread
+	 */
 	public void run() {
 		for( int i = 0; i < runs; i++ ) {
 			if( random.nextInt() % 2 == HEADS ) {
@@ -43,9 +46,10 @@ public class CoinFlip implements Runnable {
 	}
 
 	public static void main( String[] args ) {
-
 		int numOfThreads = 0,
 			numOfIterations = 0,
+			numOfIterationsPerThread = 0,
+			numOfExtraIterations = 0,
 			totalRuns = 0,
 			totalHeads = 0;
 		long startTime = System.currentTimeMillis(),
@@ -56,6 +60,7 @@ public class CoinFlip implements Runnable {
 			System.exit( 0 );
 		}
 
+		// parse arguments from command line
 		try {
 			numOfThreads = Integer.parseInt( args[0] );
 			numOfIterations = Integer.parseInt( args[1] );
@@ -64,11 +69,22 @@ public class CoinFlip implements Runnable {
 			System.exit( 0 );
 		}
 
+		// calculate iterations per thread
+		numOfIterationsPerThread = numOfIterations / numOfThreads;
+		if( numOfIterations % numOfThreads != 0 ) {
+			numOfExtraIterations = numOfIterations - numOfThreads * numOfIterationsPerThread;
+		}
+
+		// create threads and start calculations
 		ArrayList<Thread> threads = new ArrayList<Thread>( numOfThreads );
 		ArrayList<CoinFlip> coins = new ArrayList<CoinFlip>( numOfThreads );
 		for( int i = 0; i < numOfThreads; i++ )
 		{
-			CoinFlip coin = new CoinFlip( numOfIterations );
+			// handle adding remaining extra threads to last thread
+			if( numOfExtraIterations > 0 && i == numOfThreads - 1 ) {
+				numOfIterationsPerThread += numOfExtraIterations;
+			}
+			CoinFlip coin = new CoinFlip( numOfIterationsPerThread );
 			Thread thread = new Thread( coin );
 
 			coins.add( coin );
@@ -77,6 +93,7 @@ public class CoinFlip implements Runnable {
 			thread.start();
 		}
 
+		// wait for threads to finish and calculate head count
 		for( int i = 0; i < numOfThreads; i++ )
 		{
 			Thread thread = threads.get( i );
