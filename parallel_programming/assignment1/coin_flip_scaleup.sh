559c2a77fdbd3caf82cbf8e07b06365d3f53a851
Terence Lee <hone02@gmail.com>
2008-09-30 21:27:35 -0400

    * realized needed coin_flip_scaleup to vs. coin_flip_speedup

---
+++ parallel_programming/assignment1/coin_flip_scaleup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Merge the standard output and standard error
+#$ -j y
+
+#Execute the job from the current working directory
+#$ -cwd
+
+#Specify that the interpreting shell for this job is the Bash shell
+#$ -S /bin/bash
+
+#Execute the initialization script
+. $HOME/.bashrc
+
+#echo "#Iteration	Threads	ElapsedTime"
+# iteration_max_multiplier
+for(( j=1; j<=$2; j++ ));
+do
+	# iteration counts
+	for(( i=1; i<=20; i++ ));
+	do
+		let ITERATIONS=10000000*j
+		java CoinFlip $1 $j $ITERATIONS $i $3
+	done
+done
