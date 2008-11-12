9fe135da11aecc05500ac140a6d04d85bb4e4cda
hone <hone02@gmail.com>
2008-11-12 00:39:15 -0500

    * works on a single processor for 1 step * build file only runs on 1 processor for now

--- parallel_programming/assignment3/gameoflife.cpp
+++ parallel_programming/assignment3/gameoflife.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 const int DIMENSIONS = 16;
+const int DEAD = 0;
+const int ALIVE = 1;
+int empty_row[ DIMENSIONS ] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 int global_grid[ DIMENSIONS * DIMENSIONS ] = {
 	0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -21,8 +24,75 @@ int global_grid[ DIMENSIONS * DIMENSIONS ] = {
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 };
 
+// calculates the offset of the array
+int offset( int x, int y )
+{
+	return x + ( y * DIMENSIONS );
+}
+
+// returns the number of alive cells in the neighboring cells
+int count_alive_neighbors( int x, int y, int * rows )
+{
+	int count = 0;
+
+	for( int i = -1; i <= 1; i++ )
+	{
+		for( int j = -1; j <= 1; j++ )
+		{
+			int new_x = x + i;
+			int new_y = y + j;
+			int value;
+			if( new_x < 0 || new_y < 0 || new_x >= DIMENSIONS || new_y >= DIMENSIONS || ( i == 0 && j == 0 ) )
+				value = 0;
+			else
+				value = rows[offset( new_x, new_y )];
+
+			count += value;
+		}
+	}
+
+	return count;
+}
+
+// checks surrounding neighboord and returns true or false if it should be flipped
+bool check_neighborhood( int x, int y, int * rows )
+{
+	int alive_count = count_alive_neighbors( x, y, rows );
+	if( rows[offset( x, y )] == DEAD )
+	{
+		if( alive_count == 3 )
+			return true;
+	}
+	else
+	{
+		if( alive_count == 2 || alive_count == 3 )
+			return true;
+	}
+
+	return false;
+}
+
+// process the rows of the grid for an interation
+int * process_rows( int * rows, int amount_to_process )
+{
+	int * new_rows = new int[amount_to_process * DIMENSIONS];
+
+	for( int i = 1; i < amount_to_process; i++ )
+	{
+		for( int x = 0; x < DIMENSIONS; x++ )
+		{
+			if( check_neighborhood( x, i, rows ) )
+				new_rows[offset( x, i - 1 )] = ALIVE;
+			else
+				new_rows[offset( x, i - 1 )] = DEAD;
+		}
+	}
+
+	return new_rows;
+}
+
 // prints the grid
-void print_grid( int iteration, int * gloabl_grid )
+void print_grid( int iteration, int * grid )
 {
 	std::cout << "Iteration " << iteration << ":\n";
 
@@ -30,7 +100,7 @@ void print_grid( int iteration, int * gloabl_grid )
 	{
 		for( int x = 0; x < DIMENSIONS; x++ )
 		{
-			std::cout << global_grid[ x + ( y * DIMENSIONS ) ] << " ";
+			std::cout << grid[offset( x, y )] << " ";
 		}
 		std::cout << std::endl;
 	}
@@ -51,9 +121,38 @@ int main( int argc, char ** argv )
 	if( ID == 0 )
 	{
 		print_grid( 0, global_grid );
+		int * rows = new int[DIMENSIONS * ( DIMENSIONS + 2 )];
+		// prepare global grid
+		for( int i = 0; i < DIMENSIONS; i++ )
+		{
+			for( int x = 0; x < DIMENSIONS; x++ )
+			{
+				rows[offset( x, i + 1 )] = global_grid[offset( x, i )];
+			}
+		}
+		// set first and last rows as empty
+		for( int i = 0 ; i < 2; i++ )
+		{
+			int y = 0;
+			if( i == 1 )
+				y = DIMENSIONS + 1;
+
+			for( int x = 0; x < DIMENSIONS; x++ )
+			{
+				rows[offset( x, y )] = empty_row[x];
+			}
+		}
+
+		int * new_rows = process_rows( rows, DIMENSIONS );
+		print_grid( 1, new_rows );
+		delete[] rows;
+		rows = NULL;
+		delete[] new_rows;
+		new_rows = NULL;
 	}
 
 	MPI_Finalize();
 
 	delete[] buffer;
+	buffer = NULL;
 }
