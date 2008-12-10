bee20da6c9dc6bc856ae7c68ccd79fd430cde7c2
Terence Lee <hone02@gmail.com>
2008-12-10 13:28:09 -0500

    * working facing detection

--- computer_vision/face_detect/face_detect.cpp
+++ computer_vision/face_detect/face_detect.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <highgui.h>
+#include <image_raii.hpp>
 #include <face_detect.hpp>
 
+using namespace std;
+
+const char* cascade_name = "haarcascade_frontalface_alt2.xml";
 CvHaarClassifierCascade * cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
 CvMemStorage* storage = cvCreateMemStorage(0);
 
-void detect_and_draw( IplImage * img, double scale )
+void detect_and_draw( IplImage * img, const char * filename, int min_neighbor, int flag, double scale )
 {
 	static CvScalar colors[] = {
 		{{0,0,255}}, {{0,128,255}}, {{0,255,255}}, {{0,255,0}},
@@ -19,7 +25,7 @@ void detect_and_draw( IplImage * img, double scale )
 
 	// Detect Objects if any
 	cvClearMemStorage( storage );
-	CvSeq * objects = cvHaarDetectObjects( small_img, cascade, storage, 1.1, 2, 0 /* CV_HAAR_DO_CANNY_PRUNING */, cvSize( 30, 30 ) );
+	CvSeq * objects = cvHaarDetectObjects( small_img, cascade, storage, 1.1, min_neighbor, flag, cvSize( 10, 10 ) );
 
 	// Loop through found objects and draw boxes around them
 	for( int i = 0; i < (objects ? objects->total : 0); i++ )
@@ -27,11 +33,46 @@ void detect_and_draw( IplImage * img, double scale )
 		CvRect * r = (CvRect*)cvGetSeqElem( objects, i );
 		cvRectangle(
 				img,
-				cvPoint( r.x, r.y ),
-				cvPoint( r.x + r.width, r.y + r.height ),
+				cvPoint( r->x, r->y ),
+				cvPoint( r->x + r->width, r->y + r->height ),
 				colors[i % 8]
-				)
+				);
 	}
+	cvSaveImage( filename, img );
+
 	cvReleaseImage( &gray );
 	cvReleaseImage( &small_img );
+}
+
+vector< string > tokenize_str( const string & str, const string & delims )
+{
+    // Skip delims at beginning, find start of first token
+    std::string::size_type lastPos = str.find_first_not_of(delims, 0);
+    // Find next delimiter @ end of token
+    std::string::size_type pos     = str.find_first_of(delims, lastPos);
+
+    // output vector
+    std::vector< std::string > tokens;
+
+    while (std::string::npos != pos || std::string::npos != lastPos)
+    {
+        // Found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        // Skip delims.  Note the "not_of". this is beginning of token
+        lastPos = str.find_first_not_of(delims, pos);
+        // Find next delimiter at end of token.
+        pos     = str.find_first_of(delims, lastPos);
+    }
+
+    return tokens;
+}
+
+int main( int argc, char ** argv )
+{
+	ImageRAII image( argv[1] );
+	int neighbor = lexical_cast< string, int >( argv[3] );
+	int flag = lexical_cast< string, int >( argv[4] );
+	detect_and_draw( image.image, argv[2], neighbor, flag, 1.0 );
+
+	return 0;
 }
--- computer_vision/face_detect/face_detect.hpp
+++ computer_vision/face_detect/face_detect.hpp
@@ -1,4 +1,8 @@
 #include <cv.h>
+#include <vector>
+#include <string>
+#include <sstream>
+
 // Page 511 to 512 of the O'Reilly Learning OpenCV book
 // Detect and draw detected object boxes on image
 // Presumes 2 Globals:
@@ -6,4 +10,24 @@
 //		cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
 //	AND that storage is allocated:
 //	CvMemStorage* storage = cvCreateMemStorage(0);
-void detect_and_draw( IplImage * img, double scale = 1.3 );
+void detect_and_draw( IplImage * img, const char * filename, double scale = 1.3 );
+
+/**
+ * String tokenizer taken from http://www.rosettacode.org/wiki/Tokenizing_A_String#C.2B.2B
+ */
+std::vector<std::string> tokenize_str( const std::string & str, const std::string & delims );
+
+/**
+ * Typecasts from source to destination.
+ */
+template< typename source, typename dest>
+dest lexical_cast(source src)
+    {
+        std::stringstream ss;
+        dest output;
+
+        ss << src;
+        ss >> output;
+    
+        return output;
+    }
