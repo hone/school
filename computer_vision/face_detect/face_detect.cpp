80dcb9f61ee6512d13ca546a2b5f5aa715cd8ae5
Terence Lee <hone02@gmail.com>
2008-12-10 15:54:42 -0500

    * extract_faces replaces detect and draw and now is a library of class FaceDetect

--- computer_vision/face_detect/face_detect.cpp
+++ computer_vision/face_detect/face_detect.cpp
@@ -1,78 +1,52 @@
 #include <iostream>
 #include <highgui.h>
 #include <image_raii.hpp>
+#include <window_raii.hpp>
 #include <face_detect.hpp>
 
 using namespace std;
 
-const char* cascade_name = "haarcascade_frontalface_alt2.xml";
-CvHaarClassifierCascade * cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
-CvMemStorage* storage = cvCreateMemStorage(0);
-
-void detect_and_draw( IplImage * img, const char * filename, int min_neighbor, int flag, double scale )
+FaceDetect::FaceDetect( string cascade_filename )
 {
-	static CvScalar colors[] = {
-		{{0,0,255}}, {{0,128,255}}, {{0,255,255}}, {{0,255,0}},
-		{{255,128,0}},{{255,255,0}}, {{255,0,0}}, {{255,0,255}}
-	}; // just some pretty colors to draw with
+	this->cascade_name = cascade_filename;
+	this->cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name.c_str(), 0, 0, 0 );
+	this->storage = cvCreateMemStorage(0);
+}
+
+FaceDetect::~FaceDetect()
+{
+	cvReleaseHaarClassifierCascade( &(this->cascade) );
+	cvReleaseMemStorage( &(this->storage) );
+}
+
+vector<IplImage *> FaceDetect::extract_faces( IplImage * img, int scale_size, int min_neighbor, int flag, double scale )
+{
+	vector<IplImage *> faces;
 
 	// Image Preparation
-	IplImage * gray = cvCreateImage( cvSize( img->width, img->height ), 8, 1 );
-	IplImage * small_img = cvCreateImage( cvSize( cvRound( img->width / scale ), cvRound( img->height / scale ) ), 8, 1 );
-	cvCvtColor( img, gray, CV_BGR2GRAY );
-	cvResize( gray, small_img, CV_INTER_LINEAR );
-	cvEqualizeHist( small_img, small_img );
+	ImageRAII gray( cvCreateImage( cvSize( img->width, img->height ), 8, 1 ) );
+	ImageRAII small_img( cvCreateImage( cvSize( cvRound( img->width / scale ), cvRound( img->height / scale ) ), 8, 1 ) );
+	cvCvtColor( img, gray.image, CV_BGR2GRAY );
+	cvResize( gray.image, small_img.image, CV_INTER_LINEAR );
+	cvEqualizeHist( small_img.image, small_img.image );
 
 	// Detect Objects if any
 	cvClearMemStorage( storage );
-	CvSeq * objects = cvHaarDetectObjects( small_img, cascade, storage, 1.1, min_neighbor, flag, cvSize( 10, 10 ) );
+	CvSeq * objects = cvHaarDetectObjects( small_img.image, cascade, storage, 1.1, min_neighbor, flag, cvSize( 10, 10 ) );
 
 	// Loop through found objects and draw boxes around them
 	for( int i = 0; i < (objects ? objects->total : 0); i++ )
 	{
 		CvRect * r = (CvRect*)cvGetSeqElem( objects, i );
-		cvRectangle(
-				img,
-				cvPoint( r->x, r->y ),
-				cvPoint( r->x + r->width, r->y + r->height ),
-				colors[i % 8]
-				);
+		ImageRAII face( cvCreateImage( cvSize( r->width, r->height ), 8, 1 ) );
+		IplImage * face_resize = cvCreateImage( cvSize( scale_size, scale_size ), 8, 1 );
+
+		// extract face from picture
+		cvSetImageROI( gray.image, *r );
+		cvCopy( gray.image, face.image );
+		cvResize( face.image, face_resize );
+		faces.push_back( face_resize );
 	}
-	cvSaveImage( filename, img );
 
-	cvReleaseImage( &gray );
-	cvReleaseImage( &small_img );
-}
-
-vector< string > tokenize_str( const string & str, const string & delims )
-{
-    // Skip delims at beginning, find start of first token
-    std::string::size_type lastPos = str.find_first_not_of(delims, 0);
-    // Find next delimiter @ end of token
-    std::string::size_type pos     = str.find_first_of(delims, lastPos);
-
-    // output vector
-    std::vector< std::string > tokens;
-
-    while (std::string::npos != pos || std::string::npos != lastPos)
-    {
-        // Found a token, add it to the vector.
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
-        // Skip delims.  Note the "not_of". this is beginning of token
-        lastPos = str.find_first_not_of(delims, pos);
-        // Find next delimiter at end of token.
-        pos     = str.find_first_of(delims, lastPos);
-    }
-
-    return tokens;
-}
-
-int main( int argc, char ** argv )
-{
-	ImageRAII image( argv[1] );
-	int neighbor = lexical_cast< string, int >( argv[3] );
-	int flag = lexical_cast< string, int >( argv[4] );
-	detect_and_draw( image.image, argv[2], neighbor, flag, 1.0 );
-
-	return 0;
+	return faces;
 }
--- computer_vision/face_detect/face_detect.hpp
+++ computer_vision/face_detect/face_detect.hpp
@@ -1,33 +1,30 @@
+#ifndef _FACE_DETECT_H
+#define _FACE_DETECT_H
+
 #include <cv.h>
 #include <vector>
 #include <string>
 #include <sstream>
+#include <string>
 
-// Page 511 to 512 of the O'Reilly Learning OpenCV book
-// Detect and draw detected object boxes on image
-// Presumes 2 Globals:
-//  Cascade is loaded by:
-//		cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
-//	AND that storage is allocated:
-//	CvMemStorage* storage = cvCreateMemStorage(0);
-void detect_and_draw( IplImage * img, const char * filename, double scale = 1.3 );
+class FaceDetect
+{
+	private:
+		std::string cascade_name;
+		CvHaarClassifierCascade * cascade;
+		CvMemStorage * storage;
 
-/**
- * String tokenizer taken from http://www.rosettacode.org/wiki/Tokenizing_A_String#C.2B.2B
- */
-std::vector<std::string> tokenize_str( const std::string & str, const std::string & delims );
+	public:
+		FaceDetect( std::string cascade_filename = "haarcascade_frontalface_alt2.xml" );
+		~FaceDetect();
+		// Page 511 to 512 of the O'Reilly Learning OpenCV book
+		// Detect and draw detected object boxes on image
+		// Presumes 2 Globals:
+		//  Cascade is loaded by:
+		//		cascade = (CvHaarClassifierCascade*)cvLoad( cascade_name, 0, 0, 0 );
+		//	AND that storage is allocated:
+		//	CvMemStorage* storage = cvCreateMemStorage(0);
+		std::vector< IplImage * > extract_faces( IplImage * img, int scale_size, int min_neighbor, int flag, double scale = 1.3 );
+};
 
-/**
- * Typecasts from source to destination.
- */
-template< typename source, typename dest>
-dest lexical_cast(source src)
-    {
-        std::stringstream ss;
-        dest output;
-
-        ss << src;
-        ss >> output;
-    
-        return output;
-    }
+#endif
